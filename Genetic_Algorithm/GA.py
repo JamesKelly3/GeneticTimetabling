a7ed6f88fea51ce3f11daf2da5cd14a8ae5517f7
JamesKelly3 <jamesakelly@hotmail.co.uk>
2017-06-02 23:15:31 +0100

    Small changes

--- Genetic_Algorithm/GA.py
+++ Genetic_Algorithm/GA.py
@@ -17,11 +17,12 @@ def ga(university: University):
     fitness = [eval_fitness(indiv) for indiv in population]
     while gen_count < max_gens and not stopping_criteria(fitness):
         elites = elitism(fitness, population)
-        parents = select(pop_size-len(elites), fitness, population)
+        parents = select(pop_size - len(elites), fitness, population)
         offspring = crossover(parents, crossover_prob)
         offspring = mutate(offspring, mutation_prob)
         population = elites + offspring
         fitness = [eval_fitness(indiv) for indiv in population]
+        gen_count += 1
 
 
 def generate_initial_population(university: University, pop_size: int):
